4d928dbf21407264319244d2e90d957a39a04a4a
Kadir <116947551+serpikoz@users.noreply.github.com>
2023-12-07 17:14:23 +0300

    categories-start

---
+++ app/(dashboard)/[storeId]/(routes)/categories/compenents/columns.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
+
+export type CategoryColumn = {
+  id: string;
+  name: string;
+  billBoardLabel: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "billboard",
+    header: "BillBoard",
+    cell: ({ row }) => row.original.billBoardLabel,
+  },
+
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
